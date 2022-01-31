fe4a7f841cd39758d18e3f326cdda9a458ab1823
Markus Leitner <leitnermrks@gmail.com>
2022-01-31 12:40:47 +0100

    add build to compose, change rgit links, add download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+cd data
+wget -O verefine_data.tar.gz "https://tuwienacat-my.sharepoint.com/:u:/g/personal/markus_j_leitner_tuwien_ac_at/EQKuNSHbI45IiVyRCg1jPccB6K-3mlRJkeBT72w0qWRrgA?download=1"
+tar -xzvf verefine_data.tar.gz
