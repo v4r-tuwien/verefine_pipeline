11a175d1bd76da4a40522ca3fb531af34150c0c1
Markus Leitner <leitnermrks@gmail.com>
2022-05-30 17:33:03 +0200

    update download link

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 cd data
-wget -O verefine_data.tar.gz "https://tuwienacat-my.sharepoint.com/:u:/g/personal/markus_j_leitner_tuwien_ac_at/EQKuNSHbI45IiVyRCg1jPccB6K-3mlRJkeBT72w0qWRrgA?download=1"
-tar -xzvf verefine_data.tar.gz
+wget -O data.zip "https://owncloud.tuwien.ac.at/index.php/s/oyh3CeHneDlAewn/download"
+unzip data.zip
